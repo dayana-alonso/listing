4a08f462262465c4761f7b551f978c1903052b2c
Dayana <dayanalon83@gmail.com>
2018-11-22 17:22:02 +0000

    load to listing

---
+++ files.rb
@@ -0,0 +1,24 @@
+## creating files
+
+# my_file = File.new('todo.txt','w+')
+# my_file.close
+
+# puts '---------'
+
+## writing into files
+
+# File.open('todo.txt','w') {|file| file.write("learn to code with kingsley. 
+# 	A dream does not become reality through magic; it takes sweat, determination and hard work 
+# 	– Colin Powell
+# 	Who you are tomorrow begins with what you do today.	– Tim Fargo
+# 	No matter how much it hurts now, One day you will look back and realize it 
+# 	changed your life for the better")}
+
+# puts '---------'
+
+
+## reading lines
+
+# puts File.read('todo.txt')
+
+# puts '---------'
